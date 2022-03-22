88b7219a90ae6605bcf44216d7ba1d1dbaedba35
Christopher Star <59939824+starchristopher@users.noreply.github.com>
2022-03-22 19:26:45 -0300

    refactor: register feedback

--- src/components/Pages/Registro/Form/index.tsx
+++ src/components/Pages/Registro/Form/index.tsx
@@ -44,6 +44,11 @@ const RegisterForm: React.FC = () => {
         name: data.name,
       });
 
+      toast("Usuário registrado com sucesso!", {
+        progressClassName: "progress-confirmation",
+        className: "toaster-confirmation",
+      });
+
       setCurrentUser(response);
       setStatus(STATUS.CONNECTED);
     } catch (err: any) {
