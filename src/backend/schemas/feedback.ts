f590757eb70307dc24b5bb59631d963481d42923
Arthur Lopes <arthurcastroes@gmail.com>
2022-03-27 12:18:17 -0300

    refactor: validade whatsapp number

--- src/backend/schemas/feedback.ts
+++ src/backend/schemas/feedback.ts
@@ -2,7 +2,7 @@ import { object, string, TypeOf } from "yup";
 
 export const newFeedback = object({
   email: string().optional().email(),
-  whatsapp: string().optional(), // TODO: VALIDAR NUMERO
+  whatsapp: string().optional().min(10).max(11),
   service: string().required().min(6),
   name: string().required().min(3),
 });
