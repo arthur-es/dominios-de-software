62488a388185780f400683bc4dfdb71fd24a4aaa
Arthur Lopes <arthurcastroes@gmail.com>
2022-04-11 16:47:49 -0300

    fix: possible undefined

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,3 @@
-export const supabaseUrl = "https://rjhqhsvkrgkqziqndflb.supabase.co";
-export const supabaseKey = process.env.SUPABASE_KEY;
-
-export const sacflowEndpoint = process.env.SACFLOW_ENDPOINT;
-export const sacflowToken = process.env.SACFLOW_TOKEN;
-
 // Make sure the process doest not continue without essential ENVs
 if (!process.env.SUPABASE_KEY) {
   throw new Error("Check you SUPABASE_KEY env");
@@ -16,3 +10,8 @@ if (!process.env.SACFLOW_ENDPOINT) {
 if (!process.env.SACFLOW_TOKEN) {
   throw new Error("Check you SACFLOW_TOKEN env");
 }
+export const supabaseUrl = "https://rjhqhsvkrgkqziqndflb.supabase.co";
+export const supabaseKey = process.env.SUPABASE_KEY;
+
+export const sacflowEndpoint = process.env.SACFLOW_ENDPOINT;
+export const sacflowToken = process.env.SACFLOW_TOKEN;
