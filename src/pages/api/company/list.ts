3184f439938d3613c71aca06aaa52144048eed0a
Arthur Lopes <arthurcastroes@gmail.com>
2022-04-11 22:03:13 -0300

    feat: list companies

---
+++ src/pages/api/company/list.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { validate } from "../../../backend/middleware/validade";
+import { companySchema } from "../../../backend/schemas/company";
+
+import { PrismaInstance } from "../../../backend/database";
+
+const prisma = PrismaInstance.getInstance();
+
+async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const { email } = req.query;
+
+  const parsedEmail = email.toString();
+
+  try {
+    const foundCompany = await prisma.company.findMany({
+      where: { email: parsedEmail },
+    });
+
+    console.log(foundCompany);
+
+    return res.status(200).json(foundCompany);
+  } catch (err: any) {
+    console.log(err);
+
+    return res.status(400).json({ message: err.message });
+  }
+}
+
+export default validate(companySchema, handler);
