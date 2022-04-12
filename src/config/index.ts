e9185eee9bb6bc8d5d81b01550201762a5ae7742
Arthur Lopes <arthurcastroes@gmail.com>
2022-04-11 21:58:45 -0300

    refactor: envs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,9 @@
 // Make sure the process doest not continue without essential ENVs
-if (!process.env.SUPABASE_KEY) {
-  throw new Error("Check you SUPABASE_KEY env");
-}
-
-if (!process.env.SACFLOW_ENDPOINT) {
-  throw new Error("Check you SACFLOW_ENDPOINT env");
-}
-
-if (!process.env.SACFLOW_TOKEN) {
-  throw new Error("Check you SACFLOW_TOKEN env");
-}
 export const supabaseUrl = "https://rjhqhsvkrgkqziqndflb.supabase.co";
-export const supabaseKey = process.env.SUPABASE_KEY;
+export const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQxMjk4NjUxLCJleHAiOjE5NTY4NzQ2NTF9.ViFO2zvzpHYOLnqyWUE62UkBjZtYt2mzqC3HPpWay3w";
 
-export const sacflowEndpoint = process.env.SACFLOW_ENDPOINT;
-export const sacflowToken = process.env.SACFLOW_TOKEN;
+export const sacflowEndpoint = "https://hyerdev-notify-hpac.api.sacflow.io";
+export const sacflowToken = "hyerdev-notify-hpac";
 
 export const SITE_URL = "https://wptrack.vercel.app";
